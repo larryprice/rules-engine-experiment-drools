9eaf8171d0a5e6e50589d9368e945e5805652824
Larry Price <larry.price.dev@gmail.com>
2014-12-12 14:32:14 -0500

    moving some code around

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,17 +11,24 @@ import org.kie.api.runtime.KieSession;
 
 @RestController
 public class GreetingController {
-    private final AtomicLong counter = new AtomicLong();
+    private KieContainer kieContainer = null;
+
+    public GreetingController() {
+        KieServices ks = KieServices.Factory.get();
+        kieContainer = ks.getKieClasspathContainer();
+    }
+
+    public void runRules(Greeting g) {
+        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+        kieSession.insert(g);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name, @RequestParam(value="language", defaultValue="english") String language) {
         Greeting g = new Greeting(1, name, language);
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kContainer = ks.getKieClasspathContainer();
-        KieSession kSession = kContainer.newKieSession("ksession-rules");
-        kSession.insert(g);
-        kSession.fireAllRules();
-        kSession.dispose();
+        runRules(g);
 
         return g;
     }
